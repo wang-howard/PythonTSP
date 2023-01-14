297f49c3a4b4b579524cc7042590de89a47983ce
wang-howard <105531941+wang-howard@users.noreply.github.com>
2023-01-13 23:07:43 -0500

    Created smallest insertion heuristic client

---
+++ SmallestInsertion.py
@@ -0,0 +1,24 @@
+"""
+SmallestInsertion.py
+Author: Howard Wang
+
+Takes a file from standard in containing coordinates of points to be
+added to a tour using the smallest insertion heuristic. Prints the number
+of points in the tour and the total length.
+"""
+
+from Tour import Tour
+from Tour import Point
+
+tour = Tour()
+
+fileName = input("Name of file: ")
+with open(fileName, 'r') as f:
+    for line in f:
+        line = line.strip()
+        coords = line.split(" ")
+        p = Point(float(coords[0]), float(coords[1]))
+        tour.insertSmallest(p)
+
+print("Number of Points: " + str(tour.size()))
+print("Tour Length: " + str(tour.length()))
