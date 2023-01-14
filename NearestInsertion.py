f7253a89a7e423489613656f6b9fe16a5d8d7db2
wang-howard <105531941+wang-howard@users.noreply.github.com>
2023-01-13 23:07:55 -0500

    debugging to nearest insertion heuristic code

--- NearestInsertion.py
+++ NearestInsertion.py
@@ -6,13 +6,19 @@ Takes a file from standard in containing coordinates of points to be
 added to a tour using the nearest insertion heuristic. Prints the number
 of points in the tour and the total length.
 """
-from Tour.py import Tour
-from Point import Point
-fileName = input("Name of file: ")
+
+from Tour import Tour
+from Tour import Point
+
 tour = Tour()
 
+fileName = input("Name of file: ")
 with open(fileName, 'r') as f:
     for line in f:
         line = line.strip()
         coords = line.split(" ")
+        p = Point(float(coords[0]), float(coords[1]))
+        tour.insertNearest(p)
 
+print("Number of Points: " + str(tour.size()))
+print("Tour Length: " + str(tour.length()))
--- main.py
+++ main.py
@@ -7,8 +7,8 @@ points. The value of the tour length is then compared to a Java
 implementation of TSP that is confirmed to be correctly implemented.
 """
 
-from Point import Point
 from Tour import Tour
+from Tour import Point
 
 a = Point(621.0, 400.0)
 b = Point(420.0, 512.0)
@@ -27,11 +27,11 @@ print("Tour:")
 print(test)
 
 test.insertNearest(a)
-test.insertSmallest(b)
-test.insertSmallest(c)
-test.insertSmallest(d)
-test.insertSmallest(e)
-test.insertSmallest(f)
+test.insertNearest(b)
+test.insertNearest(c)
+test.insertNearest(d)
+test.insertNearest(e)
+test.insertNearest(f)
 
 print("After Insertion:")
 print("Number of points = " + str(test.size()))
