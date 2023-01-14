9b21c0235f63fdc575fa170bb157441af3d46861
wang-howard <105531941+wang-howard@users.noreply.github.com>
2023-01-13 22:40:19 -0500

    Began nearest insertion file

--- NearestInsertion.py
+++ NearestInsertion.py
@@ -6,4 +6,13 @@ Takes a file from standard in containing coordinates of points to be
 added to a tour using the nearest insertion heuristic. Prints the number
 of points in the tour and the total length.
 """
+from Tour.py import Tour
+from Point import Point
+fileName = input("Name of file: ")
+tour = Tour()
+
+with open(fileName, 'r') as f:
+    for line in f:
+        line = line.strip()
+        coords = line.split(" ")
 
